841c34e95cdb55c09f4128cd4e05f371f2f0caa8
Maksym Kundeev <mkundeev@gmail.com>
2022-06-27 13:10:42 +0300

    proptypes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -37,7 +37,12 @@ Profile.propTypes = {
     username: PropTypes.string.isRequired,
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
-    stats: PropTypes.objectOf(PropTypes.number).isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+
+    }).isRequired,
 };
 
 
